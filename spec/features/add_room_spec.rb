3f9a1550872dc2458688c088f901890357fdaca9
Austin Wood <i@austinwood.me>
2015-06-16 14:21:08 -0600

    Uncomment login step now that it's working properly

--- spec/features/add_room_spec.rb
+++ spec/features/add_room_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe "Authenticated User" do
   it "can add a chat room" do
     page.visit root_path
-    #user = mock_omniauth_user
+    mock_omniauth_user
 
-    #click_on "Login with GitHub"
+    click_on "Login with GitHub"
     click_link_or_button "Create a new room"
 
     page.within("#new-room-form") do
