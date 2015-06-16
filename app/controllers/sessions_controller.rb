a7f7d78b359fa7420cf154937c4f4600167715a1
Austin Wood <i@austinwood.me>
2015-06-16 14:22:00 -0600

    Update our sessions_controller to handle both login (with GitHub) and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,15 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_or_create_from_auth(request.env["omniauth.auth"])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path
+      flash[:notice] = "Welcome #{user.name}!"
+    end
+  end
+
+  def destroy
+    session.clear
     redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "dashboard#index"
 
   get "/auth/:provider/callback", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :rooms
 end
