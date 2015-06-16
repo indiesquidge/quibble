8aaaa840a051c18900d309456712639230a9e1be
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-06-16 17:17:01 -0600

    fixed @indiesquidge 's error. Now it's finally working right again... Austin.

--- spec/features/chat_room_spec.rb
+++ spec/features/chat_room_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 RSpec.describe "User in a chatroom", type: :feature do
   it "sees a list of recent messages" do
-    chatroom1 = Room.create!(name: "lonely")
-    mes_1 = chatroom1.messages.create!(body: "Hey guys what's up")
-    mes_2 = chatroom1.messages.create!(body: "I'm so alone")
-    mes_3 = chatroom1.messages.create!(body: "I wish someone would join us.")
-    mes_4 = chatroom1.messages.create!(body: "Okay, well, goodbye")
+    chatroom = Room.create!(name: "lonely")
+    mes_1 = chatroom.messages.create!(body: "Hey guys what's up")
+    mes_2 = chatroom.messages.create!(body: "I'm so alone")
+    mes_3 = chatroom.messages.create!(body: "I wish someone would join us.")
+    mes_4 = chatroom.messages.create!(body: "Okay, well, goodbye")
 
-    page.visit "/room/lonely"
+    page.visit room_path(chatroom)
     page.within("#recent-messages") do
       expect(page).to have_content(mes_1.body)
       expect(page).to have_content(mes_2.body)
