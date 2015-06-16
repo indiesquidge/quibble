2414ad67b97850fbfbb3791dd19e8f86697cede4
Kyra Steenbock <kyra.m.steenboc@gmail.com>
2015-06-16 07:43:40 -0600

    implemented room title in room#show

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,6 +3,22 @@ class RoomsController < ApplicationController
   end
 
   def create
-binding.pry
+    room = Room.new(room_params)
+    if room.save
+      redirect_to room_path(room)
+    else
+      flash[:error] = "An error prevented your room from being saved."
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
--- spec/features/add_room_spec.rb
+++ spec/features/add_room_spec.rb
@@ -3,16 +3,20 @@ require 'rails_helper'
 describe "Authenticated User" do
   it "can add a chat room" do
     page.visit root_path
-#    user = mock_omniauth_user
+#user = mock_omniauth_user
 
-  #  click_on "Login with GitHub"
+  #click_on "Login with GitHub"
     click_link_or_button "Create a new room"
 
     page.within("#new-room-form") do
       expect(page).to have_content("Room name:")
     end
 
-    fill_in("room[name]", with: "testing suite")
+    fill_in("room[name]", with: "Testing Suite")
     click_on "Create room"
+
+    page.within("#room-show") do
+      expect(page).to have_content("Testing Suite")
+    end
   end
 end
